d61b0f186d6b25bb95cf087289238ef5ad3f851d
Godofredo Bravo <godofredo.bravo.plaza@gmail.com>
2026-07-27 23:08:14 -0400

    fix: correct firebase config import path in finance app

--- projects/finance/js/app.js
+++ projects/finance/js/app.js
@@ -1,4 +1,4 @@
-import { auth, db } from '../../firebase-config.js';
+import { auth, db } from '../../../firebase-config.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { collection, addDoc, getDocs, deleteDoc, doc, query, where } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
